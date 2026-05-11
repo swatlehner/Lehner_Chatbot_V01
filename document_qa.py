2f7718d7cbf36782b17392806dc6d3efa7436087
sjwetzel <sebastian.wetzel@gmx.net>
2026-05-11 12:44:26 +0200

    question language

--- document_qa.py
+++ document_qa.py
@@ -159,7 +159,7 @@ def ask_gemini_stream(query, session_id):
             - If the answer is not in the document, say something helpful.
             - Do NOT invent values
             - Be precise with numbers (especially tables)
-            - Answer in the same language as the user
+            - Answer in the same language as the QUESTION
             
             CHAT HISTORY:
             {history_text}
